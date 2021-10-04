45cc0a6aec42487b10c98212c9eb8c38dc67f98c
Brevan Bender <bcaybender@gmail.com>
2021-10-04 15:35:44 -0700

    Added music and fixed some bugs feeling good

--- app.js
+++ app.js
@@ -1,19 +1,53 @@
+//Set constants
+const music = document.getElementById('music')
 const btn = document.getElementById('button');
 let response = document.getElementById('response');
+const instructions = document.getElementById('instructions');
+const refresh = document.getElementById('restart');
+const giveup = document.getElementById('giveup');
 
-let number = [Math.floor(Math.random() * 20)]
-
+//Set variable numbers
+let number = [Math.floor(Math.random() * 20)];
 let lives = 4 
 
 btn.addEventListener('click' , function(){
+    //set the spooky tunes
     let input = document.getElementById('guess1').value;
+    music.volume = 0.1;
+    music.play();
+    //adjust lives
     lives--;
-    console.log(number)
+    //If you win
     if (input == number){
-        response.innerHTML = `You enter room ${number} and find the secret passage. You race through tight corridors and make it out alive! You have ${lives} more tries.`;
-    } else if (input < number){
+        response.innerHTML = `You enter room ${number} and find the secret passage. You race through tight corridors and make it out alive! Press the restart button to play again`;
+        guess1.style.display = "none" ;
+        button.style.display = "none" ;
+        giveup.style.display = "none" ;
+        
+    //If input is too high
+    } else if (Number(input) > number){
         response.innerHTML = `You enter rooom ${input}. While frantically searching you find a note that says "To escape search rooms with a lower number". You have ${lives} more tries. `;
-    } else {
+    
+    //If unput is too low
+    
+    } else if (Number(input) < number){
         response.innerHTML = `You enter room ${input}. A terrifying goul chases you out. Your gut is telling you to search in higher room numbers. You have ${lives} more tries.`;
     }
+    instructions.style.display = "none";
+    
+    //if you run out of lives
+    if (lives <= 0) {
+        guess1.style.display = "none";
+        button.style.display = "none";
+        ghost.style.display = "block";
+        failure.style.display = "block";
+        response.innerHTML = "Oh no! The ghosts caught you. How spooky? Press restart to try again";
+    }
 });
+
+    //giveup button
+giveup.addEventListener('click' , () =>{
+    response.innerHTML = `Giving up so easy? The escape was in room ${number} Press restart to try again.`;
+    instructions.style.display = "none";
+
+})
